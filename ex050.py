8a612d2d4580b1b79d3ad3b9881d50918639e1be
jeongho lee <ljho1898@naver.com>
2020-12-06 00:31:29 +0900

    ex050

---
+++ ex050.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+data = "0309490    "
+data=data.rstrip()
+print(data)
+# 오른쪽 공백을 제거하기 위해서는 rstrip() 메서드를 사용하면 된다.
+# 답 0309490  (오른쪽의 공백이 없음)
+
+
+# In[ ]:
+
+
+
+
