02b05bcc96f22aa93c9b1cbbfb0bf42dea448233
jeongho lee <ljho1898@naver.com>
2020-12-06 00:55:57 +0900

    ex110

---
+++ ex110.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+
+#두번째 if 가 참이면 1,2가 나올것이거 거짓이면 3이나온다. 여기서 두번째 if는 거짓이기 떄문에 3이 나온다.
+#그리고 첫번째 i문은 끝났기 때문에 마지막 5가 나온다.
+#따라서 답은 
+#            3
+#            5 
+# 가 나올것이다
+
+
+# In[ ]:
+
+
+
+
