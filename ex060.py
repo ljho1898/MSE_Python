421a3d3cad060c715949d28c8e781d8407aca684
jeongho lee <ljho1898@naver.com>
2020-12-06 00:33:14 +0900

    ex060

---
+++ ex060.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nums = [1,2,3,4,5]
+average = sum(nums)/len(nums)
+print(average)
+
+# 평균을 구하기 위해서는 (수의 합)/(수의 개수) 로 하면 된다.
+# 답 3.0
+
+
+# In[ ]:
+
+
+
+
