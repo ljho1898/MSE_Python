1e460c3d0ca2df89caec0651a9de50532978e34a
jeongho lee <ljho1898@naver.com>
2020-12-06 00:29:32 +0900

    ex030

---
+++ ex030.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+string = 'abcds'
+b=string.replace('d','J')
+print(b)
+
+#  replace를 사용하면 문자열을 변경하여 나타나게 된다.
+# 답 abcJs
+
+
+# In[ ]:
+
+
+
+
