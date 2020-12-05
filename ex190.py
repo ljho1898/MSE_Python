f092c64cfe2c762e899fe468eae96f250d09173e
jeongho lee <ljho1898@naver.com>
2020-12-06 01:03:37 +0900

    ex190

---
+++ ex190.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+apart = [ [101, 102], [201, 202], [301, 302] ]
+for row in apart:
+    for col in row:
+        print(col, "호")
+print("-" * 5)
+
+# apart에 fow가 있고 row에 col이 있다고 하고 print로 뒤에 호를 붙인다. 그리고 for문을 나가게 된다.
+# 답 101 호
+#    102 호
+#    201 호
+#    202 호
+#    301 호
+#    302 호
+-----
+
+
+# In[ ]:
+
+
+
+
