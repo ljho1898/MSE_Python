a50e95ab8ed9d95b77dec72e3c82a4610eea346f
jeongho lee <ljho1898@naver.com>
2020-12-06 01:08:22 +0900

    ex250

---
+++ ex250.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy
+for i in numpy.arange(0, 5, 0.1):
+    print(i)
+# 0에서 5까지 0.1씩 증가하기 위해서는 numpy가 필요하다. 
+#답
+# 0.0
+# 0.1
+#  .
+#  .
+#  4.9
+
+
+# In[ ]:
+
+
+
+
