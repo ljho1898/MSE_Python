c222eaf2924359fb081c255c8db008d2b16ceb81
jeongho lee <ljho1898@naver.com>
2020-12-06 00:33:52 +0900

    ex070

---
+++ ex070.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data = [2,4,3,1,5,10,9]
+data.sort()
+print(data)
+
+#오름차순으로 정렬하기 위해서는 sort를 사용 하면 된다.
+# 답 [1,2,3,4,5,9,10]
+
+
+# In[ ]:
+
+
+
+
