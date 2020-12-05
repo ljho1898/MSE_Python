30036cca4d4d8179fcdd8c8987782bae08e72620
jeongho lee <ljho1898@naver.com>
2020-12-06 00:30:41 +0900

    ex040

---
+++ ex040.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data="  삼성전자  "
+data1=data.strip()
+print(data1)
+
+# 좌우의 공백을 제거하기 위해서는 strip()메서드를 사용 하면 된다.
+# 답 삼성전자 (좌우 공백이 없음)
+
+
+# In[ ]:
+
+
+
+
