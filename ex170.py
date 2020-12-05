48174632bd933ad10e44ebfb7643d0a4313f7574
jeongho lee <ljho1898@naver.com>
2020-12-06 01:02:19 +0900

    ex170

---
+++ ex170.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+result = 1
+for i in range(1, 11) :
+    result *= i
+print(result)
+
+# 범위가 1부터 10이기에 range(1,11)이고 result 를 계속 곱하기 위해 for문을 사용한다. 
+#이렇게 10까지 다 곱한뒤 for문을 빠져나오게 된다.
+#답 3628800
+
+
+# In[ ]:
+
+
+
+
