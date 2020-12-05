d41638598339b698f5c8cd7d350b182c7e3ccc7c
jeongho lee <ljho1898@naver.com>
2020-12-06 01:09:01 +0900

    ex260

---
+++ ex260.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class OMG : 
+    def print() :
+        print("Oh my god")
+
+myStock = OMG()
+myStock.print()
+
+# 위에 print()에는 인자갑이 없다.  그런데 아래 myStock.print()는 
+# OMG.print(mystock)을 이렇게 호출하게 되어서 인자가 없는데 인자가 하나 넘어와서 오류가 발생하는 것이다.
+
+
+# In[ ]:
+
+
+
+
