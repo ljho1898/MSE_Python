9225093b87e4be7da0ae8374056a55486b91badf
jeongho lee <ljho1898@naver.com>
2020-12-06 00:35:10 +0900

    ex090

---
+++ ex090.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']
+Traceback (most recent call last):
+    File "<pyshell#69>", line 1, in <module>
+    icecream['누가바']
+KeyError: '누가바'
+    
+#  딕셔너리에 없는 키를 사용했기 때문에 오류가 발생한 것이다.
+
+
+# In[ ]:
+
+
+
+
